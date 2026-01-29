c7f6fc2f99c94303fc07d2810ec9a2dfc6895fc7
tran-ethan <tran.ethan@hotmail.com>
2026-01-29 18:15:04 -0500

    Setup Spring Boot Initializr project with maven

---
+++ backend/src/test/java/ca/mcgill/ecse321/group1/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
