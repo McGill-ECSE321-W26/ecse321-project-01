0bc0f9a3e870dc9941cd778c400b815f99b5bdc8
Minh Vo <minh.vo2@mail.mcgill.ca>
2026-02-03 00:53:44 -0500

    add spotless for autoformat and spotbugs for unused variable check

--- backend/src/main/java/ca/mcgill/ecse321/group1/controller/PersonController.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/controller/PersonController.java
@@ -2,33 +2,31 @@ package ca.mcgill.ecse321.group1.controller;
 
 import ca.mcgill.ecse321.group1.model.Person;
 import ca.mcgill.ecse321.group1.service.PersonService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/person")
 @RestController
 public class PersonController {
 
-    private final PersonService personService;
+  private final PersonService personService;
 
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+  public PersonController(PersonService personService) {
+    this.personService = personService;
+  }
 
-    @GetMapping
-    public List<Person> getPeople() {
-        return personService.getPeople();
-    }
+  @GetMapping
+  public List<Person> getPeople() {
+    return personService.getPeople();
+  }
 
-    @GetMapping("{username}")
-    public Person getPersonByUsername(@PathVariable String username) {
-        return personService.getPersonById(username);
-    }
+  @GetMapping("{username}")
+  public Person getPersonByUsername(@PathVariable String username) {
+    return personService.getPersonById(username);
+  }
 
-    @PostMapping
-    public void addPerson(@RequestBody Person person) {
-        personService.insertPerson(person);
-    }
-
+  @PostMapping
+  public void addPerson(@RequestBody Person person) {
+    personService.insertPerson(person);
+  }
 }
