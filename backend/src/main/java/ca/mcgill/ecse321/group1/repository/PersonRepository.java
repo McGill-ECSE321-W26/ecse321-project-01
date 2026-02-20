454feb7f3652fb45cd44f59dda35e6781c376afa
tran-ethan <tran.ethan@hotmail.com>
2026-02-20 00:35:50 -0500

    Add tests

--- backend/src/main/java/ca/mcgill/ecse321/group1/repository/PersonRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/repository/PersonRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
     Person findPersonByPersonID(String id);
+    Person findPersonByEmail(String email);
 }
