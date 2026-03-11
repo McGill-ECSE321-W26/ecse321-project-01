4cb41cd604b774cefbd6822451d97d5a5e8d564f
Gnardisitor <bajanica.dragos@gmail.com>
2026-03-11 18:17:47 -0400

    Initial order service and get mappings for order controller

--- backend/src/main/java/ca/mcgill/ecse321/group1/repository/ItemRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/repository/ItemRepository.java
@@ -1,8 +1,11 @@
 package ca.mcgill.ecse321.group1.repository;
 
+import ca.mcgill.ecse321.group1.model.Customer;
 import ca.mcgill.ecse321.group1.model.Item;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<Item, String> {
   Item findItemByItemID(String itemID);
+
+  Iterable<Item> findItemsByCustomer(Customer customer);
 }
--- backend/src/main/java/ca/mcgill/ecse321/group1/repository/OrderRepository.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package ca.mcgill.ecse321.group1.repository;
 
+import ca.mcgill.ecse321.group1.model.Customer;
 import ca.mcgill.ecse321.group1.model.Order;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Integer> {
+import java.util.List;
 
+public interface OrderRepository extends CrudRepository<Order, Integer> {
   Order findOrderByOrderID(String id);
+
+  List<Order> findByCustomer(Customer customer);
+
+  List<Order> findByOrderStatus(Order.OrderStatus orderStatus);
 }
