cf41321cd4ada5c0b4771969248f8b9e4e6bad79
stupidavid <tangdavid03@gmail.com>
2026-03-13 18:09:56 -0400

    added the test file and the proposed methods i will implement

---
+++ backend/src/test/java/ca/mcgill/ecse321/group1/service/AccountServiceTests.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.group1.service;
+
+public class AccountServiceTests {
+}
+/*
+* Testing Account:
+
+createAccount
+logIn
+logOut
+getAccountByID
+getAccounts
+getCartByAccountID
+
+switchAccount (different account types or accounts)
+updateAccountPassword (optional ?)
+updateAccountName (optional ?)
+
+*
+* */
