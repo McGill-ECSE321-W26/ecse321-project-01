5c7491b9a4e22a645300a27f6d6da4e4e411c300
Minh Vo <minh.vo2@mail.mcgill.ca>
2026-03-13 21:47:02 -0400

    current test

--- backend/src/test/java/ca/mcgill/ecse321/group1/service/ClothingServiceTests.java
+++ backend/src/test/java/ca/mcgill/ecse321/group1/service/ClothingServiceTests.java
@@ -1,4 +1,39 @@
 package ca.mcgill.ecse321.group1.service;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.group1.model.ClothingModel;
+import ca.mcgill.ecse321.group1.model.ClothingVariant;
+import ca.mcgill.ecse321.group1.model.Customer;
+import ca.mcgill.ecse321.group1.model.Employee;
+import ca.mcgill.ecse321.group1.model.Item;
+import ca.mcgill.ecse321.group1.model.Order;
+import ca.mcgill.ecse321.group1.repository.CustomerRepository;
+import ca.mcgill.ecse321.group1.repository.EmployeeRepository;
+import ca.mcgill.ecse321.group1.repository.ItemRepository;
+import ca.mcgill.ecse321.group1.repository.OrderRepository;
+
+@SpringBootTest
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
 public class ClothingServiceTests {
+
 }
