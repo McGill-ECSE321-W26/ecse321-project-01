a94d78c5d2f564b22d6f6d39d41d4f5490b46d14
Bassam Baki <bassam.baki@mail.mcgill.ca>
2026-03-14 17:40:55 -0400

    person controller and dtos completed, password hashing added

--- backend/src/main/java/ca/mcgill/ecse321/group1/controller/PersonController.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/controller/PersonController.java
@@ -1,11 +1,17 @@
 package ca.mcgill.ecse321.group1.controller;
 
+import ca.mcgill.ecse321.group1.dto.AddressDto;
+import ca.mcgill.ecse321.group1.dto.CreateCustomerDto;
+import ca.mcgill.ecse321.group1.dto.CreateEmployeeDto;
+import ca.mcgill.ecse321.group1.dto.LoginDto;
+import ca.mcgill.ecse321.group1.dto.PersonResponseDto;
+import ca.mcgill.ecse321.group1.dto.UpdatePasswordDto;
+import ca.mcgill.ecse321.group1.model.Customer;
 import ca.mcgill.ecse321.group1.model.Person;
 import ca.mcgill.ecse321.group1.service.PersonService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/person")
 @RestController
@@ -17,8 +23,41 @@ public class PersonController {
     this.personService = personService;
   }
 
-  @GetMapping
-  public Iterable<PersonResponseDto> getPeople() {
+  @PostMapping("customer")
+  public PersonResponseDto createCustomer(@RequestBody CreateCustomerDto dto) {
+    Person p =
+        personService.createCustomer(
+            dto.getId(), dto.getEmail(), dto.getPassword(), dto.getAddress());
+    return new PersonResponseDto(p);
+  }
+
+  @PostMapping("employee")
+  public PersonResponseDto createEmployee(@RequestBody CreateEmployeeDto dto) {
+    Person p = personService.createEmployee(dto.getId(), dto.getEmail(), dto.getPassword());
+    return new PersonResponseDto(p);
+  }
+
+  @PostMapping("{id}/add-customer-role")
+  public PersonResponseDto addCustomerRoleToEmployee(
+      @PathVariable String id, @RequestBody AddressDto dto) {
+    Person p = personService.addCustomerRoleToEmployee(id, dto.getAddress());
+    return new PersonResponseDto(p);
+  }
+
+  @PostMapping("{id}/add-employee-role")
+  public PersonResponseDto addEmployeeRoleToCustomer(@PathVariable String id) {
+    Person p = personService.addEmployeeRoleToCustomer(id);
+    return new PersonResponseDto(p);
+  }
+
+  @PostMapping("login")
+  public PersonResponseDto logIn(@RequestBody LoginDto dto) {
+    Person p = personService.logIn(dto.getEmail(), dto.getPassword(), dto.getRole());
+    return new PersonResponseDto(p);
+  }
+
+  @GetMapping("")
+  public List<PersonResponseDto> getPeople() {
     List<PersonResponseDto> dtos = new ArrayList<>();
     for (Person p : personService.getPeople()) {
       dtos.add(new PersonResponseDto(p));
@@ -31,23 +70,27 @@ public class PersonController {
     return new PersonResponseDto(personService.getPersonById(id));
   }
 
-  @PostMapping
-  public PersonResponseDto addPerson(@RequestBody PersonRequestDto request) {
-    Person p = personService.insertPerson(
-            request.getId(),
-            request.getEmail(),
-            request.getPassword()
-    );
+  @PutMapping("{id}/password")
+  public PersonResponseDto updatePassword(
+      @PathVariable String id, @RequestBody UpdatePasswordDto dto) {
+    Person p = personService.updatePassword(id, dto.getOldPassword(), dto.getNewPassword());
     return new PersonResponseDto(p);
   }
 
-  @PostMapping("/login")
-  public PersonResponseDto logIn(@RequestBody PersonRequestDto request) {
-    Person p = personService.logIn(
-            request.getEmail(),
-            request.getPassword(),
-            request.getRole()
-    );
-    return new PersonResponseDto(p);
+  @PutMapping("customer/{customerRoleId}/address")
+  public PersonResponseDto updateCustomerAddress(
+      @PathVariable String customerRoleId, @RequestBody AddressDto dto) {
+    Customer c = personService.updateCustomerAddress(customerRoleId, dto.getAddress());
+    return new PersonResponseDto(c.getPerson());
+  }
+
+  @DeleteMapping("{id}")
+  public void deleteAccount(@PathVariable String id) {
+    personService.deleteAccount(id);
+  }
+
+  @DeleteMapping("{id}/self")
+  public void deleteSelfAccount(@PathVariable String id) {
+    personService.deleteSelfAccount(id);
   }
 }
--- backend/src/main/java/ca/mcgill/ecse321/group1/dto/PersonResponseDto.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/dto/PersonResponseDto.java
@@ -1,37 +1,78 @@
-package ca.mcgill.ecse321.group1.controller;
+package ca.mcgill.ecse321.group1.dto;
 
-
+import ca.mcgill.ecse321.group1.model.Customer;
+import ca.mcgill.ecse321.group1.model.Employee;
+import ca.mcgill.ecse321.group1.model.Manager;
 import ca.mcgill.ecse321.group1.model.Person;
+import ca.mcgill.ecse321.group1.model.PersonRole;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonResponseDto {
-    private String id;
-    private String email;
+  private String id;
+  private String email;
+  private List<String> roleTypes;
+  private String address;
+  private Integer loyaltyPoints;
 
-    // Jackson needs a default constructor, but it doesn't need to be public
-    @SuppressWarnings("unused")
-    private PersonResponseDto() {
+  // Jackson needs a default constructor, but it doesn't need to be public
+  @SuppressWarnings("unused")
+  private PersonResponseDto() {}
+
+  public PersonResponseDto(Person model) {
+    this.id = model.getPersonID();
+    this.email = model.getEmail();
+    this.roleTypes = new ArrayList<>();
+    for (PersonRole role : model.getRoles()) {
+      if (role instanceof Customer customer) {
+        this.roleTypes.add("Customer");
+        this.address = customer.getAddress();
+        this.loyaltyPoints = customer.getLoyaltyPoints();
+      } else if (role instanceof Manager) {
+        this.roleTypes.add("Manager");
+      } else if (role instanceof Employee) {
+        this.roleTypes.add("Employee");
+      }
     }
+  }
 
-    public PersonResponseDto(Person model) {
-        this.id = model.getPersonID();
-        this.email = model.getEmail();
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public List<String> getRoleTypes() {
+    return roleTypes;
+  }
 
+  public String getAddress() {
+    return address;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public Integer getLoyaltyPoints() {
+    return loyaltyPoints;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setRoleTypes(List<String> roleTypes) {
+    this.roleTypes = roleTypes;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setLoyaltyPoints(Integer loyaltyPoints) {
+    this.loyaltyPoints = loyaltyPoints;
+  }
 }
