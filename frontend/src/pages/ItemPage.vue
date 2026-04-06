f49d7ff6b3a020709086fc96315cc9d371a3aefe
santipadron <santi.padron@yahoo.com>
2026-04-05 22:59:50 -0400

    more work on the item page. have to figure out how to render the different colors and sizes

--- frontend/src/pages/ItemPage.vue
+++ frontend/src/pages/ItemPage.vue
@@ -4,7 +4,7 @@ import type { ClothingModelResponseDto, ClothingVariantResponseDto } from '@/api
 import { ref, onMounted, computed } from 'vue'
 import { useRoute, RouterLink } from 'vue-router'
 import { Button } from '@/components/ui/button'
-import { ArrowLeft } from 'lucide-vue-next'
+import { ArrowLeft, ChevronDown } from 'lucide-vue-next'
 
 const route = useRoute()
 
@@ -15,6 +15,7 @@ const modelId = computed(() => route.params.id as string)
 const model = ref<ClothingModelResponseDto | null>(null)
 const variants = ref<ClothingVariantResponseDto[]>([])
 const currVariant = ref<ClothingVariantResponseDto | null>(null)
+const detailsOpen = ref(false)
 
 onMounted(async () => {
   try {
@@ -30,7 +31,11 @@ onMounted(async () => {
 })
 
 function addToCart() {
+  // TODO
+}
 
+function toggleDetails() {
+  detailsOpen.value = !detailsOpen.value
 }
 
 
@@ -42,11 +47,11 @@ function addToCart() {
     <div class="max-w-350 mx-auto px-5 md:px-10 py-6 pt-16 md:pt-24 flex items-center gap-3 flex-wrap">
       <RouterLink to="/shop">
         <Button
-          size="lg"
+          size="sm"
           variant="outline"
-          class="rounded-none border-(--text-light) text-[20px] tracking-wide bg-transparent text-(--text-muted) hover:bg-(--card-hover) hover:border-(--text-muted)"
+          class="rounded-none border-(--text-light) text-[18px] tracking-wide bg-transparent text-(--text-light) hover:bg-(--card-hover) hover:border-(--text-muted) p-1"
         >
-          <ArrowLeft :size="20" :strokeWidth="4" /> Back to shopping
+          <ArrowLeft :size="10" :strokeWidth="3" /> Back to shopping
         </Button>
       </RouterLink>
     </div>
@@ -78,6 +83,51 @@ function addToCart() {
         No variants found.
       </p>
     </div>
+
+    <!-- Show item -->
+     <div
+      v-else
+      class="grid grid-cols-1 md:grid-cols-2 pb-10"
+     >
+      <!-- image -->
+      <div class="aspect-[3/4] w-full overflow-hidden bg-(--card-hover)">
+        <img
+          :src="currVariant?.imagePath"
+          :alt="model?.name"
+          loading="lazy"
+          class="w-full h-full object-cover"
+        >
+      </div>
+
+      <!-- info -->
+      <div class="pt-8 px-6">
+        <!-- item name -->
+        <p class="text-[40px]">
+          {{ model?.name }}
+        </p>
+        <!-- colors -->
+        
+        <!-- sizes -->
+
+        <!-- add to cart button -->
+        <Button
+          size="lg"
+          class="rounded-none text-[20px] font-medium text-(--bg) py-6 bg-[#AE5A31] hover:bg-[#773f23]"
+          @click="addToCart">
+          Add to Cart &nbsp {{ model?.price }}$
+        </Button>
+        <!-- details -->
+        <Button
+          @click="toggleDetails">
+          <span>Details</span>
+          <ChevronDown class="transition-transform duration-200" :class="{ 'rotate-180': detailsOpen }"/>
+        </Button>
+
+        <div v-show="detailsOpen" class="mt-3 text-(--text-muted) leading-relaxed">
+          {{ model?.description }}
+        </div>
+      </div>
+     </div>
 
   </div>
 </template>
