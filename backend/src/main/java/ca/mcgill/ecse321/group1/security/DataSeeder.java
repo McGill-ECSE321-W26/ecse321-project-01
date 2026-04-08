dce2484a5378d23cc148d88b00fb44197a0eeb8e
jayjay4387 <gazon4387@gmail.com>
2026-04-07 21:18:31 -0400

    removed unused variables

--- backend/src/main/java/ca/mcgill/ecse321/group1/security/DataSeeder.java
+++ backend/src/main/java/ca/mcgill/ecse321/group1/security/DataSeeder.java
@@ -187,9 +187,7 @@ public class DataSeeder {
             "#3a2728",
             10,
             "https://dam.dynamiteclothing.com/m/17817c7723843918/original/100096517_8F4_alt3_1920x2880.jpg?sw=740&sh=1110");
-    ClothingVariant dressFloralBrownS = dressFloralBrown[0],
-        dressFloralBrownM = dressFloralBrown[1],
-        dressFloralBrownL = dressFloralBrown[2];
+
     // Braided Belt — brown, olive, beige
     ClothingModel braidedBelt =
         createClothingModel(
